3474ac5564f2cf806ef49fa949a67997b75bbaac
hmq <gl34daisy@163.com>
2022-07-22 22:36:32 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,146 +39,136 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   menuList = [
     {
-      title: 'Home',
+      title: '首页',
       icon: 'home',
-      link: '/home',
+      link: '/welcome',
     },
     {
-      title: 'Dashboard',
-      icon: 'dashboard',
-      open: false,
-      children: [
-        {
-          title: '分析页',
-          link: '/welcome'
-        },
-        {
-          title: '监控页',
-          link: '/dashboard/monitor'
-        },
-        {
-          title: '工作台',
-          link: '/dashboard/workplace'
-        },
-      ]
-    },
-    {
-      title: '表单页',
-      icon: 'form',
-      open: false,
-      children: [
-        {
-          title: '基本表单',
-          link: '/form/basic-form'
-        },
-        {
-          title: '分步表单',
-          link: '/form/step-form'
-        },
-        {
-          title: '高级表单',
-          link: '/form/advanced-form'
-        },
-      ]
-    },
-    {
-      title: '列表页',
-      icon: 'table',
-      open: false,
-      children: [
-        {
-          title: '搜索列表',
-          open: false,
-          children: [
-            {
-              title: '搜索列表（文章）',
-              link: '/list/search/articles'
-            },
-            {
-              title: '搜索列表（项目）',
-              link: '/list/search/projects'
-            },
-            {
-              title: '搜索列表（应用）',
-              link: '/list/search/applications'
-            },
-          ]
-        },
-        {
-          title: '查询表格',
-          link: '/list/table-list'
-        },
-        {
-          title: '标准列表',
-          link: '/list/basic-list'
-        },
-        {
-          title: '卡片列表',
-          link: '/list/card-list'
-        },
-      ]
-    },
-    {
-      title: '详情页',
+      title: '菜单一',
       icon: 'profile',
       open: false,
       children: [
         {
-          title: '基础详情页',
-          link: '/profile/basic'
+          title: '目录1',
+          open: false,
+          children: [
+            {
+              title: '页面1',
+              link: '/11'
+            },
+            {
+              title: '页面2',
+              link: '/12'
+            },
+          ]
         },
         {
-          title: '高级详情页',
-          link: '/profile/advanced'
-        },
-      ]
-    },
-    {
-      title: '结果页',
-      icon: 'check-circle',
-      open: false,
-      children: [
-        {
-          title: '成功页',
-          link: '/result/success'
-        },
-        {
-          title: '失败页',
-          link: '/result/fail'
-        },
-      ]
-    },
-    {
-      title: '异常页',
-      icon: 'warning',
-      open: false,
-      children: [
-        {
-          title: '403',
-          link: '/exception/403'
+          title: '目录2',
+          open: false,
+          children: [
+            {
+              title: '页面1',
+              link: '/21'
+            },
+            {
+              title: '页面2',
+              link: '/22'
+            },
+          ]
         },
         {
-          title: '404',
-          link: '/exception/404'
+          title: '页面1',
+          link: '/1'
         },
         {
-          title: '500',
-          link: '/exception/500'
+          title: '页面2',
+          link: '/2'
         },
       ]
     },
     {
-      title: '个人页',
-      icon: 'user',
+      title: '菜单二',
+      icon: 'profile',
       open: false,
       children: [
         {
-          title: '个人中心',
-          link: '/account/center'
+          title: '目录1',
+          open: false,
+          children: [
+            {
+              title: '页面1',
+              link: '/11'
+            },
+            {
+              title: '页面2',
+              link: '/12'
+            },
+          ]
         },
         {
-          title: '个人设置',
-          link: '/account/settings'
+          title: '目录2',
+          open: false,
+          children: [
+            {
+              title: '页面1',
+              link: '/21'
+            },
+            {
+              title: '页面2',
+              link: '/22'
+            },
+          ]
+        },
+        {
+          title: '页面1',
+          link: '/1'
+        },
+        {
+          title: '页面2',
+          link: '/2'
+        },
+      ]
+    },
+    {
+      title: '菜单三',
+      icon: 'profile',
+      open: false,
+      children: [
+        {
+          title: '目录1',
+          open: false,
+          children: [
+            {
+              title: '页面1',
+              link: '/11'
+            },
+            {
+              title: '页面2',
+              link: '/12'
+            },
+          ]
+        },
+        {
+          title: '目录2',
+          open: false,
+          children: [
+            {
+              title: '页面1',
+              link: '/21'
+            },
+            {
+              title: '页面2',
+              link: '/22'
+            },
+          ]
+        },
+        {
+          title: '页面1',
+          link: '/1'
+        },
+        {
+          title: '页面2',
+          link: '/2'
         },
       ]
     },
